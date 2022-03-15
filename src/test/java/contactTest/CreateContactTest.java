e07cae3c9fdce73c5ba614b6c23915225a7a1fa3
91834 <91834@NARAYANA>
2022-03-15 13:03:17 +0530

    Neeraj first commite

---
+++ src/test/java/contactTest/CreateContactTest.java
@@ -0,0 +1,12 @@
+package contactTest;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+
+	@Test
+	public void createContactTest()
+	{
+		System.out.println("contact  created");
+	}
+}
---
+++ src/test/java/orgTest/CreateOrgTest.java
@@ -0,0 +1,18 @@
+package orgTest;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+	@Test
+	public void createOrgTest()
+	{
+		System.out.println("org test created");
+	}
+	
+	@Test
+	public void createOrgTest1()
+	{
+		System.out.println("org test 1 created");
+	}
+
+}
